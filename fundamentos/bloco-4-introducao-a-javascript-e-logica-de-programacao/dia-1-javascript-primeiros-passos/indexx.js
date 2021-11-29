382533cef203fb2ae115ddb5904e003c74fbe36c
Patrick Barbosa <patrickafb2020@gmail.com>
2021-11-29 18:21:50 -0300

    iniciando dia 2 js

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/indexx.js
@@ -0,0 +1,60 @@
+
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(i=0; i < names.length; i+=1){
+    console.log(names[i]);
+}
+
+
+
+
+
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push('peito de peru')
+// let menuServices = menu;
+
+// console.log(menuServices);
+
+
+
+
+
+
+
+// const b = 60;
+// const a = 60;
+// const c = 60;
+
+// let l1 = 60;
+// let l2 = 60;
+// let l3 = 60;
+
+// if (l1 === l2 && l1 ===l3 && l1 + l2 + l3 == 180) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+// if (a > 0){
+//     console.log("positive")
+// } else {
+//     console.log("negative")
+// }
+
+
+
+
+// (a > b && c ? console.log(a) : console.log(b) );
+
+
+
+
+
+// if (a > b){
+//     console.log(a);
+// } else {
+//     console.log(b);
+// }
