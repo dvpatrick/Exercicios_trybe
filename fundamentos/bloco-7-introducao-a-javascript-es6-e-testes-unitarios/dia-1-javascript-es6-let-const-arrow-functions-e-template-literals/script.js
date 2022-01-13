29ecaec8aba8f52fa14309562a09755639e87caf
Patrick Barbosa <patrickafb2020@gmail.com>
2022-01-13 13:39:18 -0300

    atualizando sub pastas

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,67 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+
+function verificador(){
+    for(let i=0; i<=5; i++){
+        // console.log(oddsAndEvens[i])
+        // console.log("1")
+
+        for(let o=1; o<=5; o++){
+            // if(oddsAndEvens[i] >= oddsAndEvens[o]){
+            //     console.log(`${oddsAndEvens[i]} e maior que ${oddsAndEvens[o]}`)
+            // } else{
+            //     console.log(`${oddsAndEvens[i]} e menor que ${oddsAndEvens[o]}`)
+            // };
+        // console.log("1")
+        }
+        console.log(oddsAndEvens[i])
+                    if(oddsAndEvens[i] >= oddsAndEvens[o]){
+                console.log(`${oddsAndEvens[i]} e maior que ${oddsAndEvens[o]}`)
+            } else{
+                console.log(`${oddsAndEvens[i]} e menor que ${oddsAndEvens[o]}`)
+            };
+
+    }
+}
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+// console.log(verificador);
+// let teste = true
+// if(teste==true){
+//     verificador
+// }
+
+// let body1 = document.querySelector('body').style
+
+// function teste1(){
+//     console.log("loooggg")
+//     body1.backgroundColor = 'black';
+
+// }
+
+let btn = document.querySelector(".botao");
+
+// btn.addEventListener(onclick, teste1());
+
+
+
+
+
+
+
+
+
+
+// testingScope(true);
+
+
+// function testingScope(escopo) {
+//     if (escopo === true) {
+//       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = ' ótimo, fui utilizada no escopo !';
+//       console.log(ifScope);
+//     } else {
+//       let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//   }
