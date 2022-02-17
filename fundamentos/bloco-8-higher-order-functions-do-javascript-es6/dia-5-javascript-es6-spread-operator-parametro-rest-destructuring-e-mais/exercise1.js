db2ca9936f24edcfc851803d444cfd7343db98a8
dvpatrick <patrickafb2020@gmail.com>
2022-02-17 13:07:11 -0300

    push so pra atualizar pacotes sem envios

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise1.js
@@ -0,0 +1,90 @@
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// escreva filterPeople abaixo
+
+const filterPeople = (array) => {
+  let verifyBornIn =
+}
+
+
+
+
+
+
+
+
+
+// const alex = {
+//   name: 'Alex',
+//   age: 26,
+//   likes: ['fly fishing'],
+//   nationality: 'Australian',
+// };
+
+// const gunnar = {
+//   name: 'Gunnar',
+//   age: 30,
+//   likes: ['hiking', 'scuba diving', 'taking pictures'],
+//   nationality: 'Icelandic',
+// };
+
+// // complete a assinatura da função abaixo
+// const personLikes = ({name, age, likes}) =>{
+// return `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+// }
+
+// console.log(personLikes(alex)) ;  // 'Alex is 26 years old and likes fly fishing.'
+// console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+
+
+
+
+
+
+
+
+
+// const rectangleArea = (width, height) => width * height;
+
+// const rectangle1 = [1, 2];
+// const rectangle2 = [3, 5];
+// const rectangle3 = [6, 9];
+// const rectangles = [rectangle1, rectangle2, rectangle3];
+
+// rectangles.forEach((rectangle) => {
+  
+ 
+//   rectangleArea() // altere a chamada da funcao rectangleArea
+//   // console.log(rectangle[0] * rectangle[1]);
+// });
+// let naxew = [1, 3, 7];
+// const sum = (...numbers) => numbers.reduce((acumulator, curr) => acumulator =+ curr);
+
+// console.log(sum([1,  2])); 
